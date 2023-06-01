609d1cfb3741c5fd84344beff0255a09f9f43cb3
Jun Kim <jun7.kim@gmail.com>
2023-06-01 15:28:43 +0900

    [feature] 37p

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -7,6 +7,10 @@ public:
 	vector<int> of(int i)
 	{
 		vector<int> resutl = {};
+		if (i==2)
+		{
+			resutl.push_back(2);
+		}
 		return resutl;
 	}
 };
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(TestCaseName, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
